3335d4b7d06a93ec072555fa6885d31d9901f8fb
koma00 <koma199604@gmail.com>
2022-02-02 18:36:09 +0200

    Add tests to project

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def test_user(self):
+        username = 'user'
+        u = User(username=username)
+        self.assertEqual(u.username, username)
