5f373dc0d309d66548240bbbf8a32ccdb14d98ac
koma00 <koma199604@gmail.com>
2022-02-14 23:54:41 +0200

    Added index view

--- app/core/tests.py
+++ app/core/tests.py
@@ -7,3 +7,15 @@ class UserTestCase(TestCase):
         username = 'user'
         u = User(username=username)
         self.assertEqual(u.username, username)
+
+
+class IndexViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/index.html')
+
