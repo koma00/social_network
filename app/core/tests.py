1320e10ee58303adeaa12bed7dc0e7ef67fc78e6
koma00 <koma199604@gmail.com>
2022-02-14 23:59:17 +0200

    Fix linting error

--- app/core/tests.py
+++ app/core/tests.py
@@ -18,4 +18,3 @@ class IndexViewTest(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/index.html')
-
