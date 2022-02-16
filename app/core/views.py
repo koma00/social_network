d7c171d017b702edfb16fc9e3959aacb5927f3b9
Oleksandr Kolomoiets <koma199604@gmail.com>
2022-02-16 18:33:38 +0200

    Add custom 404 page (#13)

--- app/core/views.py
+++ app/core/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+def error_404(request, exception):
+    context = {}
+    return render(request, 'core/404.html', context)
