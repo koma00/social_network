b9df75ef1954dc683de482196b2f70f49d81c59c
Александр Коломоец <koma00@MacBook-Air-Aleksandr.local>
2023-02-06 22:27:35 +0200

    change template

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,45 +1,24 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('edit-account-setting', views.edit_account_setting, name='edit-account-setting'),
-    path('edit-interest', views.edit_interest, name='edit-interest'),
-    path('edit-password', views.edit_password, name='edit-password'),
+    path('contact', views.contact, name='contact'),
     path('edit-profile-basic', views.edit_profile_basic, name='edit-profile-basic'),
-    path('edit-work-eductation', views.edit_work_eductation, name='edit-work-eductation'),
-    path('faq', views.faq, name='faq'),
-    path('groups', views.groups, name='groups'),
-    path('inbox', views.inbox, name='inbox'),
-    path('landing', views.landing, name='landing'),
-    path('logout', views.logout, name='logout'),
-    path('messages', views.messages, name='messages'),
+    path('edit-profile-interests', views.edit_profile_interests, name='edit-profile-interests'),
+    path('edit-profile-password', views.edit_profile_password, name='edit-profile-password'),
+    path('edit-profile-settings', views.edit_profile_settings, name='edit-profile-settings'),
+    path('edit-profile-work-edu', views.edit_profile_work_edu, name='edit-profile-work-edu'),
+    path('index-register', views.index_register, name='index-register'),
+    path('index', views.index, name='index'),
+    path('newsfeed-friends', views.newsfeed_friends, name='newsfeed-friends'),
+    path('newsfeed-images', views.newsfeed_images, name='newsfeed-images'),
+    path('newsfeed-messages', views.newsfeed_messages, name='newsfeed-messages'),
+    path('newsfeed-people-nearby', views.newsfeed_people_nearby, name='newsfeed-people-nearby'),
+    path('newsfeed-videos', views.newsfeed_videos, name='newsfeed-videos'),
     path('newsfeed', views.newsfeed, name='newsfeed'),
-    path('notifications', views.notifications, name='notifications'),
-    path('page-likers', views.page_likers, name='page-likers'),
-    path('support-and-help-detail', views.support_and_help_detail, name='support-and-help-detail'),
-	path('support-and-help-search-result', views.support_and_help_search_result, name='support-and-help-search-result'),
-	path('support-and-help', views.support_and_help, name='support-and-help'),
-    path('time-line', views.time_line, name='time-line'),
+    path('timeline-about', views.timeline_about, name='timeline-about'),
+    path('timeline-album', views.timeline_album, name='timeline-album'),
     path('timeline-friends', views.timeline_friends, name='timeline-friends'),
-    path('timeline-groups', views.timeline_groups, name='timeline-groups'),
-    path('timeline-pages', views.timeline_pages, name='timeline-pages'),
-    path('timeline-photos', views.timeline_photos, name='timeline-photos'),
-    path('timeline-videos', views.timeline_videos, name='timeline-videos')
+    path('timeline', views.timeline, name='timeline')
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -1,81 +1,79 @@
-import re
-from urllib import request
+# import re
+# from urllib import request
 from django.shortcuts import render
 
-def about(request):
-    return render(request, 'core/about.html')
 
-def edit_account_setting(request):
-    return render(request, 'core/edit-account-setting.html')
+def contact(request):
+	return render(request, 'core/contact.html')
 
-def edit_interest(request):
-    return render(request, 'core/edit-interest.html')
-
-def edit_password(request):
-    return render(request, 'core/edit-password.html')
 
 def edit_profile_basic(request):
-    return render(request, 'core/edit-profile-basic.html')
+	return render(request, 'core/edit-profile-basic.html')
 
-def edit_work_eductation(request):
-    return render(request, 'core/edit-work-eductation.html')
 
-def faq(request):
-    return render(request, 'core/faq.html')
+def edit_profile_interests(request):
+    return render(request, 'core/edit-profile-interests.html')
 
-def groups(request):
-    return render(request, 'core/groups.html')
 
-def inbox(request):
-    return render(request, 'core/inbox.html')
+def edit_profile_password(request):
+	return render(request, 'core/edit-profile-password.html')
+
+
+def edit_profile_settings(request):
+	return render(request, 'core/edit-profile-settings.html')
+
+
+def edit_profile_work_edu(request):
+	return render(request, 'core/edit-profile-work-edu.html')
+
+
+def index_register(request):
+	return render(request, 'core/index-register.html')
+
 
 def index(request):
-    return render(request, 'core/index.html')
+	return render(request, 'core/index.html')
 
-def landing(request):
-    return render(request, 'core/landing.html')
 
-def logout(request):
-    return render(request, 'core/logout.html')
+def newsfeed_friends(request):
+	return render(request, 'core/newsfeed-friends.html')
 
-def messages(request):
-    return render(request, 'core/messages.html')
+
+def newsfeed_images(request):
+	return render(request, 'core/newsfeed-images.html')
+
+
+def newsfeed_messages(request):
+	return render(request, 'core/newsfeed-messages.html')
+
+
+def newsfeed_people_nearby(request):
+	return render(request, 'core/newsfeed-people-nearby.html')
+
+
+def newsfeed_videos(request):
+	return render(request, 'core/newsfeed-videos.html')
+
 
 def newsfeed(request):
-    return render(request, 'core/newsfeed.html')
+	return render(request, 'core/newsfeed.html')
 
-def notifications(request):
-    return render(request, 'core/notifications.html')
 
-def page_likers(request):
-    return render(request, 'core/page-likers.html')
+def timeline_about(request):
+	return render(request, 'core/timeline-about.html')
 
-def support_and_help_detail(request):
-    return render(request, 'core/support-and-help-detail.html')
-    
-def support_and_help_search_result(request):
-    return render(request, 'core/support-and-help-search-result.html')
-    
-def support_and_help(request):
-    return render(request, 'core/support-and-help.html')
-		
-def time_line(request):
-    return render(request, 'core/time-line.html')
+
+def timeline_album(request):
+	return render(request, 'core/timeline-album.html')
+
 
 def timeline_friends(request):
-    return render(request, 'core/timeline-friends.html')
+	return render(request, 'core/timeline-friends.html')
 
-def timeline_groups(request):
-    return render(request, 'core/timeline-groups.html')
 
-def timeline_pages(request):
-    return render(request, 'core/timeline-pages.html')
+def timeline(request):
+	return render(request, 'core/timeline.html')
 
-def timeline_photos(request):
-    return render(request, 'core/timeline-photos.html')
-
-def timeline_videos(request):
-    return render(request, 'core/timeline-videos.html')
 
 def error_404(request, exception):
     context = {}
